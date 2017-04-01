9d810d3e8dd4ec3c02bdea42ae10debb5394b061
monteith <monteith.g@gmail.com>
2017-04-01 11:55:46 -0500

    Adding isEven functions

--- src/js/util/num.js
+++ src/js/util/num.js
@@ -19,4 +19,18 @@ const random = (min, max) => Math.random() * (max - min) + min;
  */
 const randomInt = (min, max) => Math.floor(random(min, max));
 
-export {random, randomInt};
+
+/**
+ * Is an even number?
+ * @param n
+ */
+const isEven = (n) => n == parseFloat(n) ? !(n%2) : void 0;
+
+
+/**
+ * Is an even number, using strict equality to prevent coercion
+ * @param n
+ */
+const isEvenStrict = (n) => n === parseFloat(n) ? !(n%2): void 0;
+
+export {random, randomInt, isEven, isEvenStrict};
